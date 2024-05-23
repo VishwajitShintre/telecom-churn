91edf41a3f618318a1d66921b6b232c23573b0b1
VishwajitShintre <97944314+VishwajitShintre@users.noreply.github.com>
2024-05-23 20:57:08 +0530

    app.py

--- customer-churn-prediction/app.py
+++ customer-churn-prediction/app.py
@@ -41,13 +41,11 @@ def main():
 # Get the directory of the current script
     current_dir = os.path.dirname(__file__)
     image_path = os.path.join(current_dir, 'App.jpg')
-
-# Check if the file exists before loading
-     if os.path.exists(image_path):
-	     image = Image.open(image_path)
-	     st.image(image, caption='App Image')
-      else:
-	      st.error(f"Image file not found at {image_path}")
+	if os.path.exists(image_path):
+		image = Image.open(image_path)
+	        st.image(image, caption='App Image')
+         else:
+		st.error(f"Image file not found at {image_path}")
 
 
 # Rest of your Streamlit app code
